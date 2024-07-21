b3662e9f494084dfc907763996a32561af0f8f4f
zainvice <amazwings@gmail.com>
2024-07-21 19:27:58 +0800

    Version Update July 21, 2024

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,10 +10,16 @@ const Sidebar = ({ onClose, isSidebarOpen }) => {
                 <a href="/portfolio" className='m-5 cursor-pointer text-gray-100 hover:text-white hover:font-bold ease-in-out duration-300'>Portfolio</a>
                 <a href="/about" className='m-5 cursor-pointer text-gray-100 hover:text-white hover:font-bold ease-in-out duration-300'>About Me</a>
          </div>
+         
          <div className='my-40'>
             <p className='text-[25px] ml-10 font-bold text-white'>Zane &copy;</p>
             <p className='text-[12px] ml-10 font-bold text-white'>A Full Stack Designer</p>
+            <div className='flex w-full mt-2 ml-8 justify-left text-center '>
+              <a className=' w-[30px] border-2 border-white hover:animate-spin rounded-full text-white mx-2 hover:bg-white hover:text-[#02B5DC] duration-300' href="https://www.linkedin.com/in/zainvice/" target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+              <a  className=' w-[30px] border-2 border-white hover:animate-spin rounded-full text-white mx-2 hover:bg-white hover:text-[#02B5DC] duration-300' href="https://www.github.com/zainvice" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+            </div>
          </div>
+         
       </div>
     );
 };
